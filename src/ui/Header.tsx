c825fd0fa8a0ffc1c3b2b08538d3cec7b407e80e
acharyaviral <acharyaviral03@gmail.com>
2025-07-20 13:17:02 +0530

    define initail ui

---
+++ src/ui/Header.tsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+
+
+function Header() {
+  return (
+    <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
+      <Link to="/" className="tracking-widest">
+        Fast Food Co.
+      </Link>
+
+     
+    </header>
+  );
+}
+
+export default Header;
